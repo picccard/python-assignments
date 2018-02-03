dc7e02972b3797632f743c246afa8cea8bb2e2f4
picccard <eskil.uhlving@gmail.com>
2018-02-03 03:40:16 +0100

    added IINI4020, assignment 01 and 02. new folder structure.

---
+++ IINI4020/01_input_print/01_input_print.py
@@ -0,0 +1,43 @@
+"""
+
+	Title:	01_input_print.py
+	Date:	02.02.2018
+	Author:	Eskil Uhlving Larsen
+
+"""
+
+# As a convention I find it helpful to use """ for docstrings
+# and ''' for block comments.
+# In this manner you can wrap ''' around your usual docstrings without conflict.
+
+
+# Task 1
+print('Hello World!')
+print('Hello Eskil')
+name = input('What is your name? ')
+print('Hello {}'.format(name))
+
+
+# Task 2
+sumA = 1 + 3 * 3
+sumB = 1+ (5 * 3)
+sumC = (1 + 4) * 3
+import math
+radius = 8
+#  PI * r ** 2
+# float()
+sumD_area = math.pi * (radius ** 2)
+# 2 * PI * r
+sumD_circumference = 2 * math.pi * radius
+
+print('A: {}'.format(sumA))
+print('B: {}'.format(sumB))
+print('C: {}'.format(sumC))
+print('Area: {}'.format(round(sumD_area, 2)))
+print('Circumference: {}'.format(round(sumD_circumference, 2)))
+
+
+# Task 3
+number1 = float(input('Number1: '))
+number2 = float(input('Number2: '))
+print('{} + {} = {}'.format(number1, number2, (number1 + number2)))
---
+++ IINI4020/02/02_math_list_format.py
@@ -0,0 +1,78 @@
+"""
+
+	Title:	02_math_list_format.py
+	Date:	02.02.2018
+	Author:	Eskil Uhlving Larsen
+
+"""
+
+# Task 1
+days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+for day in days:
+    print(day)
+print('Lenth of {} is: {}'.format(days ,len(days)))
+days.sort() # Sorts the list
+# sorted(days) # This return the list sorted, but doesn't change the orginal list
+print('{} is now sorted'.format(days))
+
+
+# Task 2
+import math
+squard_pi = math.sqrt(math.pi)
+print(round(squard_pi, 3))
+print(round(squard_pi, 6))
+
+
+# Task 2d
+print('X    sin(x)  cos(x)')
+for x in range(-100, 101, 5): # int(start), int(stop), int(step)
+    '''
+    Put x/10 instead of x * 0.1
+    Some numbers in there will be more precise,
+    e.g.
+    3*0.1 you get 0.30000000000000004,
+    3/10 you get 0.3
+    '''
+    x /= 100
+    sinX = round(math.sin(x), 4)
+    cosX = round(math.cos(x), 4)
+    print('{}   {}  {}'.format(x, sinX, cosX))
+
+
+# Task 3
+def celciusToFar(deg):
+    return round(deg * (9/5) + 32, 2)
+
+celc = int(input('How hot is it? '))
+print(celciusToFar(celc))
+
+
+# Task 4
+number1 = float(input('Number1: '))
+number2 = float(input('Number2: '))
+
+operations = {
+    1: 'add',
+    2: 'subtract',
+    3: 'multiply',
+    4: 'devide'
+}
+for nr, opt in operations.items():
+    print('{}: {}'.format(nr, opt))
+
+while True:
+    opt_to_use = input('Chose your operation: ')
+    opt_to_use_ok = True
+    if opt_to_use == '1' or opt_to_use == operations[1]:
+        print(number1 + number2)
+    elif opt_to_use == '2' or opt_to_use == operations[2]:
+        print(number1 - number2)
+    elif opt_to_use == '3' or opt_to_use == operations[3]:
+        print(number1 * number2)
+    elif opt_to_use == '4' or opt_to_use == operations[4]:
+        print(number1 / number2)
+    else:
+        print('Operation not found, try again.')
+        opt_to_use_ok = False
+    if opt_to_use_ok:
+        break
